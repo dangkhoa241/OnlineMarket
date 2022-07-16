2d773630b0df14716dfeba8aa0bc0aca7f6952a3
unknown <tronghan05062000@gmail.com>
2022-07-16 23:02:17 +0700

    add insert product

--- shop/insert-product.php
+++ shop/insert-product.php
@@ -1,214 +1,222 @@
 <?php
 session_start();
 include('include/config.php');
-if(strlen($_SESSION['alogin'])==0)
-	{	
-header('location:index.php');
-}
-else{
-	
-if(isset($_POST['submit']))
-{
-	$category=$_POST['category'];
-	$subcat=$_POST['subcategory'];
-	$productname=$_POST['productName'];
-	$productcompany=$_POST['productCompany'];
-	$productprice=$_POST['productprice'];
-	$productpricebd=$_POST['productpricebd'];
-	$productdescription=$_POST['productDescription'];
-	$productscharge=$_POST['productShippingcharge'];
-	$productavailability=$_POST['productAvailability'];
-	$productimage1=$_FILES["productimage1"]["name"];
-	$productimage2=$_FILES["productimage2"]["name"];
-	$productimage3=$_FILES["productimage3"]["name"];
-//for getting product id
-$query=mysqli_query($con,"select max(id) as pid from products");
-	$result=mysqli_fetch_array($query);
-	 $productid=$result['pid']+1;
-	$dir="productimages/$productid";
-if(!is_dir($dir)){
-		mkdir("productimages/".$productid);
-	}
+if (strlen($_SESSION['alogin']) == 0) {
+    header('location:index.php');
+} else {
 
-	move_uploaded_file($_FILES["productimage1"]["tmp_name"],"productimages/$productid/".$_FILES["productimage1"]["name"]);
-	move_uploaded_file($_FILES["productimage2"]["tmp_name"],"productimages/$productid/".$_FILES["productimage2"]["name"]);
-	move_uploaded_file($_FILES["productimage3"]["tmp_name"],"productimages/$productid/".$_FILES["productimage3"]["name"]);
-$sql=mysqli_query($con,"insert into products(category,subCategory,productName,productCompany,productPrice,productDescription,shippingCharge,productAvailability,productImage1,productImage2,productImage3,productPriceBeforeDiscount) values('$category','$subcat','$productname','$productcompany','$productprice','$productdescription','$productscharge','$productavailability','$productimage1','$productimage2','$productimage3','$productpricebd')");
-$_SESSION['msg']="Product Inserted Successfully !!";
+    if (isset($_POST['submit'])) {
+        $url = "http://localhost:9000/api/products";
+        $name = $_POST['productName'];
+        $category = $_POST['category'];
+        $productdescription = $_POST['productDecription'];
+        $productprice = $_POST['productPrice'];
+        $productimage = $_POST['productImage'];
 
-}
+        $myObj = new stdClass();
+        $myObj->name = $name;
+        $myObj->price = $productprice;
+        $myObj->image = $productimage;
+        $myObj->description = $productdescription;
+        $myObj->category = $category;
+
+        $myJSON = json_encode($myObj);
+        $ch = curl_init();
+        $token = $_SESSION['token'];
+        $auth = 'Bearer ' . $token;
 
 
+        $headers = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'authorization: ' . $auth,
+        );
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $myJSON);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        //execute post
+        $result = curl_exec($ch);
+        // curl_close($ch);
+
+        $_SESSION['msg'] = "Product Inserted Successfully !!";
+    }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop | Insert Product</title>
-    <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-    <link type="text/css" href="css/theme.css" rel="stylesheet">
-    <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
-    <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600'
-        rel='stylesheet'>
-    <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
-    <script type="text/javascript">
-    bkLib.onDomLoaded(nicEditors.allTextAreas);
-    </script>
-
-    <script>
-    function getSubcat(val) {
-        $.ajax({
-            type: "POST",
-            url: "get_subcat.php",
-            data: 'cat_id=' + val,
-            success: function(data) {
-                $("#subcategory").html(data);
-            }
-        });
-    }
-
-    function selectCountry(val) {
-        $("#search-box").val(val);
-        $("#suggesstion-box").hide();
-    }
-    </script>
 
 
-</head>
+    <?php
+    $url = "http://localhost:9000/api/categories";
+    $myObj = new stdClass();
+    $myJSON = json_encode($myObj);
+    $ch = curl_init();
+    $token = $_SESSION['token'];
+    $auth = 'Bearer ' . $token;
 
-<body>
-    <?php include('include/header.php');?>
 
-    <div class="wrapper">
-        <div class="container">
-            <div class="row">
-                <?php include('include/sidebar.php');?>
-                <div class="span9">
-                    <div class="content">
+    $headers = array(
+        'Accept: application/json',
+        'Content-type: application/json',
+        'authorization: ' . $auth,
+    );
 
-                        <div class="module">
-                            <div class="module-head">
-                                <h3>Insert Product</h3>
-                            </div>
-                            <div class="module-body">
+    //set the url, number of POST vars, POST data
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $myJSON);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-                                <?php if(isset($_POST['submit']))
-{?>
-                                <div class="alert alert-success">
-                                    <button type="button" class="close" data-dismiss="alert">×</button>
-                                    <strong>Well done!</strong>
-                                    <?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?>
+    //execute post
+    $result = curl_exec($ch);
+    curl_close($ch);
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Shop | Insert Product</title>
+        <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link type="text/css" href="css/theme.css" rel="stylesheet">
+        <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+        <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            bkLib.onDomLoaded(nicEditors.allTextAreas);
+        </script>
+    </head>
+
+    <body>
+        <?php include('include/header.php'); ?>
+
+        <div class="wrapper">
+            <div class="container">
+                <div class="row">
+                    <?php include('include/sidebar.php'); ?>
+                    <div class="span9">
+                        <div class="content">
+
+                            <div class="module">
+                                <div class="module-head">
+                                    <h3>Insert Product</h3>
                                 </div>
-                                <?php } ?>
+                                <div class="module-body">
+
+                                    <?php if (isset($_POST['submit'])) { ?>
+                                        <div class="alert alert-success">
+                                            
+                                            <button type="button" class="close" data-dismiss="alert">×</button>
+                                            <strong>Well done!</strong>
+                                            <?php echo htmlentities($_SESSION['msg']); ?><?php echo htmlentities($_SESSION['msg'] = ""); ?>
+                                        </div>
+                                    <?php } ?>
 
 
-                                <?php if(isset($_GET['del']))
-{?>
-                                <div class="alert alert-error">
-                                    <button type="button" class="close" data-dismiss="alert">×</button>
-                                    <strong>Oh snap!</strong>
-                                    <?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+                                    <?php if (isset($_GET['del'])) { ?>
+                                        
+                                        <div class="alert alert-error">
+                                            <button type="button" class="close" data-dismiss="alert">×</button>
+                                            <strong>Oh snap!</strong>
+                                            <?php echo htmlentities($_SESSION['delmsg']); ?><?php echo htmlentities($_SESSION['delmsg'] = ""); ?>
+                                        </div>
+                                    <?php } ?>
+
+                                    <br />
+
+                                    <form class="form-horizontal row-fluid" name="insertproduct" method="post" enctype="multipart/form-data">
+                                        <div class="control-group">
+                                            <label class="control-label" for="basicinput">Category</label>
+                                            <div class="controls">
+                                                <select name="category" class="span8 tip" onChange="getSubcat(this.value);" required>
+                                                    <option value="">Select Category</option>
+                                                    <?php include('../includes/config.php');
+                                                    $category = getData('categories');
+                                                    foreach ($category as $key => $value) { ?>
+                                                        <option value="<?php echo $value->_id; ?>">
+                                                            <?php echo $value->name; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="basicinput">Product Name</label>
+                                            <div class="controls">
+                                                <input type="text" name="productName" placeholder="Enter Product Name" class="span8 tip" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="basicinput">Product Decription</label>
+                                            <div class="controls">
+                                                <input type="text" name="productDecription" placeholder="Enter Product Decription" class="span8 tip" required>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="basicinput">Product Price</label>
+                                            <div class="controls">
+                                                <input type="text" name="productPrice" placeholder="Enter Product Price" class="span8 tip" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="basicinput">Product Image</label>
+                                            <div class="controls">
+                                                <input type="text" name="productImage" placeholder="Enter Product Image" class="span8 tip" required>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="control-group">
+                                            <div class="controls">
+                                                <button type="submit" name="submit" class="btn">Insert</button>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
-                                <?php } ?>
-
-                                <br />
-
-                                <form class="form-horizontal row-fluid" name="insertproduct" method="post"
-                                    enctype="multipart/form-data">
-
-                                    <div class="control-group">
-                                        <label class="control-label" for="basicinput">Category</label>
-                                        <div class="controls">
-                                            <select name="category" class="span8 tip" onChange="getSubcat(this.value);"
-                                                required>
-                                                <option value="">Select Category</option>
-                                                <?php $query=mysqli_query($con,"select * from category");
-while($row=mysqli_fetch_array($query))
-{?>
-
-                                                <option value="<?php echo $row['id'];?>">
-                                                    <?php echo $row['categoryName'];?></option>
-                                                <?php } ?>
-                                            </select>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="control-group">
-                                        <label class="control-label" for="basicinput">Product Name</label>
-                                        <div class="controls">
-                                            <input type="text" name="productName" placeholder="Enter Product Name"
-                                                class="span8 tip" required>
-                                        </div>
-                                    </div>
-
-                                    <div class="control-group">
-                                        <label class="control-label" for="basicinput">Product Decription</label>
-                                        <div class="controls">
-                                            <input type="text" name="productDecription"
-                                                placeholder="Enter Product Decription" class="span8 tip" required>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="control-group">
-                                        <label class="control-label" for="basicinput">Product Price</label>
-                                        <div class="controls">
-                                            <input type="text" name="productPrice" placeholder="Enter Product Price"
-                                                class="span8 tip" required>
-                                        </div>
-                                    </div>
-
-                                    <div class="control-group">
-                                        <label class="control-label" for="basicinput">Product Image</label>
-                                        <div class="controls">
-                                            <input type="text" name="productImage" placeholder="Enter Product Image"
-                                                class="span8 tip" required>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="control-group">
-                                        <div class="controls">
-                                            <button type="submit" name="submit" class="btn">Insert</button>
-                                        </div>
-                                    </div>
-                                </form>
                             </div>
+
+
+
+
+
                         </div>
-
-
-
-
-
+                        <!--/.content-->
                     </div>
-                    <!--/.content-->
+                    <!--/.span9-->
                 </div>
-                <!--/.span9-->
             </div>
+            <!--/.container-->
         </div>
-        <!--/.container-->
-    </div>
-    <!--/.wrapper-->
+        <!--/.wrapper-->
 
-    <?php include('include/footer.php');?>
+        <?php include('include/footer.php'); ?>
 
-    <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-    <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
-    <script src="scripts/datatables/jquery.dataTables.js"></script>
-    <script>
-    $(document).ready(function() {
-        $('.datatable-1').dataTable();
-        $('.dataTables_paginate').addClass("btn-group datatable-pagination");
-        $('.dataTables_paginate > a').wrapInner('<span />');
-        $('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
-        $('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
-    });
-    </script>
-</body>
+        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+        <script src="scripts/datatables/jquery.dataTables.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('.datatable-1').dataTable();
+                $('.dataTables_paginate').addClass("btn-group datatable-pagination");
+                $('.dataTables_paginate > a').wrapInner('<span />');
+                $('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
+                $('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
+            });
+        </script>
+    </body>
 <?php } ?>
