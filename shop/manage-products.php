51bc2ab45371436846afcf268f629c669a660020
unknown <tronghan05062000@gmail.com>
2022-07-17 08:22:23 +0700

    manage order (not done with edit & delete)

--- shop/manage-products.php
+++ shop/manage-products.php
@@ -1,145 +1,160 @@
 <?php
 session_start();
 include('include/config.php');
-if(strlen($_SESSION['shop-login'])==0)
-	{	
-header('location:index.php');
-}
-else{
-date_default_timezone_set('Asia/Kolkata');// change according timezone
-$currentTime = date( 'd-m-Y h:i:s A', time () );
+if (strlen($_SESSION['shop-token']) == 0) {
+    header('location:index.php');
+} else {
+    // date_default_timezone_set('Asia/Kolkata'); // change according timezone
+    // $currentTime = date('d-m-Y h:i:s A', time());
+    if (isset($_GET['del'])) {
+        // mysqli_query($con, "delete from products where id = '" . $_GET['id'] . "'");
+        $_SESSION['delmsg'] = "Product deleted !!";
+        $ch = curl_init();
+        $token = $_SESSION['shop-token'];
+        $auth = 'Bearer ' . $token;
+        $headers = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'authorization: ' . $auth,
+        );
+        curl_setopt($ch, CURLOPT_URL, 'http://localhost:9000/api/products/'+$_DEL['id']);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $data = curl_exec($ch);
+        echo "<script>console.log($data);</script>";
+    }
 
-if(isset($_GET['del']))
-		  {
-		          mysqli_query($con,"delete from products where id = '".$_GET['id']."'");
-                  $_SESSION['delmsg']="Product deleted !!";
-		  }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop | Manage Products</title>
-    <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-    <link type="text/css" href="css/theme.css" rel="stylesheet">
-    <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
-    <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600'
-        rel='stylesheet'>
-</head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Shop | Manage Products</title>
+        <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link type="text/css" href="css/theme.css" rel="stylesheet">
+        <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+    </head>
 
-<body>
-    <?php include('include/header.php'); ?>
+    <body>
+        <?php include('include/header.php'); ?>
 
-    <div class="wrapper">
-        <div class="container">
-            <div class="row">
-                <?php include('include/sidebar.php'); ?>
-                <div class="span9">
-                    <div class="content">
+        <div class="wrapper">
+            <div class="container">
+                <div class="row">
+                    <?php include('include/sidebar.php'); ?>
+                    <div class="span9">
+                        <div class="content">
 
-                        <div class="module">
-                            <div class="module-head">
-                                <h3>Manage Products</h3>
-                            </div>
-                            <div class="module-body table">
-                                <?php if (isset($_GET['del'])) { ?>
-                                <div class="alert alert-error">
-                                    <button type="button" class="close" data-dismiss="alert">×</button>
-                                    <strong>Oh snap!</strong>
-                                    <?php echo htmlentities($_SESSION['delmsg']); ?><?php echo htmlentities($_SESSION['delmsg'] = ""); ?>
+                            <div class="module">
+                                <div class="module-head">
+                                    <h3>Manage Products</h3>
                                 </div>
-                                <?php } ?>
+                                <div class="module-body table">
+                                    <?php if (isset($_GET['del'])) { ?>
+                                        <div class="alert alert-error">
+                                            <button type="button" class="close" data-dismiss="alert">×</button>
+                                            <strong>Oh snap!</strong>
+                                            
+                                        </div>
+                                    <?php } ?>
 
-                                <br />
+                                    <br />
 
 
-                                <table cellpadding="0" cellspacing="0" border="0"
-                                    class="datatable-1 table table-bordered table-striped	 display" width="100%">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Product Name</th>
-                                            <th>Decription</th>
-                                            <th>Category </th>
-                                            <th>Price</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                                    <table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped	 display" width="100%">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Product Name</th>
+                                                <th>Decription</th>
+                                                <th>Category </th>
+                                                <th>Price</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
 
-                                        <?php $query = mysqli_query($con, "select products.*,category.categoryName,subcategory.subcategory from products join category on category.id=products.category join subcategory on subcategory.id=products.subCategory");
-                                            $cnt = 1;
-                                            while ($row = mysqli_fetch_array($query)) {
+                                            <?php
+                                            $ch = curl_init();
+                                            $token = $_SESSION['shop-token'];
+                                            $auth = 'Bearer ' . $token;
+                                            $headers = array(
+                                                'Accept: application/json',
+                                                'Content-type: application/json',
+                                                'authorization: ' . $auth,
+                                            );
+                                            curl_setopt($ch, CURLOPT_URL, 'http://localhost:9000/api/products/shops');
+                                            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                                            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                                            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                                            //Execute the request.
+                                            $data = curl_exec($ch);
+                                            
+                                            $json =  json_decode($data);
+                                            //echo "<script>console.log($json);</script>";
+                                            // if ($json->data >= 200 && $json->code < 300)
+                                            // {
+                                            //     $_SESSION['alogin']=$_POST['username'];
+                                            //     $_SESSION['token']=$json->data->token;
+                                            // }
+                                            curl_close($ch);
+                                            $prod = $json->data;
+                                            $l = count($prod);
+                                            for ($i = 0; $i < $l; $i++) {
+                                                // echo $users[$i]; echo "<br>";
                                             ?>
-<<<<<<< HEAD
-
                                                 <tr>
-                                                    
-                                                    <td><?php echo htmlentities($value->_id); ?></td>
-                                                    <td><?php echo htmlentities($value->name); ?></td>
-                                                    <td> <?php echo htmlentities($value->description); ?></td>
-                                                    <td><?php echo htmlentities($value->category); ?></td>
-                                                    <td><?php echo htmlentities($value->price); ?></td>
+
+                                                    <td><?php echo $prod[$i]->_id; ?></td>
+                                                    <td><?php echo $prod[$i]->name; ?></td>
+                                                    <td> <?php echo $prod[$i]->description; ?></td>
+                                                    <td><?php echo $prod[$i]->category; ?></td>
+                                                    <td><?php echo $prod[$i]->price; ?></td>
                                                     <td>
-                                                        <a href="edit-products.php?id=<?php echo $row['id'] ?>"><i class="icon-edit"></i></a>
-                                                        <a href="manage-products.php?id=<?php echo $row['id'] ?>&del=delete" onClick="return confirm('Are you sure you want to delete?')"><i class="icon-remove-sign"></i></a>
+                                                        <a href="edit-products.php?id=<?php echo $prod[$i]->_id ?>"><i class="icon-edit"></i></a>
+                                                        <a href="manage-products.php" onClick="return confirm('Are you sure you want to delete?')"><i class="icon-remove-sign"></i></a>
                                                     </td>
                                                 </tr>
-                                            <?php 
-=======
-                                        <tr>
-                                            <td><?php echo htmlentities($cnt); ?></td>
-                                            <td><?php echo htmlentities($row['productName']); ?></td>
-                                            <td><?php echo htmlentities($row['categoryName']); ?></td>
-                                            <td> <?php echo htmlentities($row['subcategory']); ?></td>
-                                            <td><?php echo htmlentities($row['productCompany']); ?></td>
-                                            <td><?php echo htmlentities($row['postingDate']); ?></td>
-                                            <td>
-                                                <a href="edit-products.php?id=<?php echo $row['id'] ?>"><i
-                                                        class="icon-edit"></i></a>
-                                                <a href="manage-products.php?id=<?php echo $row['id'] ?>&del=delete"
-                                                    onClick="return confirm('Are you sure you want to delete?')"><i
-                                                        class="icon-remove-sign"></i></a>
-                                            </td>
-                                        </tr>
-                                        <?php $cnt = $cnt + 1;
->>>>>>> refs/remotes/origin/main
+                                            <?php
                                             } ?>
 
-                                </table>
+                                    </table>
+                                </div>
                             </div>
+
+
+
                         </div>
-
-
-
+                        <!--/.content-->
                     </div>
-                    <!--/.content-->
+                    <!--/.span9-->
                 </div>
-                <!--/.span9-->
             </div>
+            <!--/.container-->
         </div>
-        <!--/.container-->
-    </div>
-    <!--/.wrapper-->
+        <!--/.wrapper-->
 
-    <?php include('include/footer.php'); ?>
+        <?php include('include/footer.php'); ?>
 
-    <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-    <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
-    <script src="scripts/datatables/jquery.dataTables.js"></script>
-    <script>
-    $(document).ready(function() {
-        $('.datatable-1').dataTable();
-        $('.dataTables_paginate').addClass("btn-group datatable-pagination");
-        $('.dataTables_paginate > a').wrapInner('<span />');
-        $('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
-        $('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
-    });
-    </script>
-</body>
+        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+        <script src="scripts/datatables/jquery.dataTables.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('.datatable-1').dataTable();
+                $('.dataTables_paginate').addClass("btn-group datatable-pagination");
+                $('.dataTables_paginate > a').wrapInner('<span />');
+                $('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
+                $('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
+            });
+        </script>
+    </body>
 <?php } ?>
